0564b01b513b9b6a7113182d23000f48b20c1e61
aphuyel <aphuyel@student.cscc.edu>
2025-04-06 17:55:28 -0400

    Modifying AlienInvasion class to call the handle event and update method for settings and ship in the game loop. Adiing up arrow to move the shipup, down arrow to move the ship down, left arrow to move the ship left, and right arrow to move the ship right.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,55 @@
+"""
+Achut Phuyel
+Lab 12
+04/06/2025
+This lab is about the alien invasion game."""
 import sys
 import pygame
+from settings import settings
+from ship import Ship
+
+class AlienInvasion:
+    def __init__(self) -> None:
+        pygame.init()
+        self.settings = settings()
+
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_w, self.settings.screen_h)
+        )
+        pygame.display.set_caption(self.settings.name)
+
+        self.bg = pygame.image.load(self.settings.bg_file)
+        self.bg = pygame.transform.scale(self.bg, 
+            (self.settings.screen_w, self.settings.screen_h)
+        )
+        self.running = True
+        self.clock = pygame.time.Clock()
+
+        self.ship = Ship(self)
+
+    def run_game(self) -> None:
+        # Game loop
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+                    pygame.quit()
+                    sys.exit()
+
+                # Handle key events for the ship
+                self.ship.handle_event(event)
+
+            # Update the ship's position based on key press
+            self.ship.update()
+
+            # Update game screen
+            self.screen.blit(self.bg, (0, 0))  # Draw background
+            self.ship.draw()  # Draw the ship
+            pygame.display.flip()  # Update the display
+
+            self.clock.tick(self.settings.FPS)  # Control the frame rate
 
 if __name__ == '__main__':
-    pass
+    ai = AlienInvasion()
+    ai.run_game()
+
