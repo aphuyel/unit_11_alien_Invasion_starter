1e745af4347bbf9a4dd0b043f47dfdf4cdc68dd3
aphuyel <aphuyel@student.cscc.edu>
2025-04-12 20:54:55 -0400

    Adding alien ship and bullet and adjusting their size

--- settings.py
+++ settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
-class settings:
+
+class Settings:
     def __init__(self) -> None:
         self.name: str = 'Alien Invasion'
         self.screen_w = 1200
@@ -10,4 +11,20 @@ class settings:
         self.ship_file = Path.cwd() / 'Assets' / 'images' / 'ship2(no bg).png'
         self.ship_w = 40
         self.ship_h = 60
-        
+        self.ship_speed = 5
+        self.starting_ship_count = 3
+
+        self.bullet_file = Path.cwd() / 'Assets' / 'images' / 'lasereBlast.png'
+        self.laser_sound = Path.cwd() / 'Assets' / 'sound' / 'laser.mp3'
+        self.bullet_speed = 7
+        self.bullet_w = 10
+        self.bullet_h = 20
+        self.bullet_amount = 5
+
+        self.alien_file = Path.cwd() / 'Assets' / 'images' / 'enemy_4.png'
+        self.alien_w = 40
+        self.alien_h = 40
+        self.fleet_speed = 1
+
+        self.fleet_direction = 3
+        self.fleet_drop_speed = 1
